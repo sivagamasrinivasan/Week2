04cc4172ac17d6eeaf483b1c7552848548ad9fdc
sivagamasrinivasan <784108@pdsb.net>
2023-09-25 10:22:06 -0400

    Fees structure class

---
+++ src/printstudent/FeeStucture.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudent;
+
+/** Fee structure branch
+ *
+ * @author kumar
+ */
+public class FeeStucture {
+    private int amount;
+
+    /**
+     * @return the amount
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
